e1f708cb870c2fd42da6441589920719bb12def7
psqhk <2462678726@qq.com>
2021-06-29 16:56:52 +0800

    制造冲突2

--- springmvc_linkman/src/LinkManController.java
+++ springmvc_linkman/src/LinkManController.java
@@ -5,8 +5,8 @@ public class LinkManController {
 
     public Result findAll(){
        System.out.println("git");
-System.out.println("我好了，你呢");
-System.out.println("不能输入密码");
+System.out.println("git号了");
+System.out.println("steam没了");
     }
 
  
