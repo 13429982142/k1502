b686ab9012a70a504a5ee4e4ab7700d798b2b56c
psqhk <2462678726@qq.com>
2021-06-29 10:03:07 +0800

    添加 删除

--- springmvc_linkman/src/LinkManController.java
+++ springmvc_linkman/src/LinkManController.java
@@ -5,9 +5,8 @@ public class LinkManController {
 
     public Result findAll(){
        System.out.println("git");
-System.out.println("我是你爹");
-System.out.println("麻了");
 System.out.println("我好了，你呢");
+System.out.println("不能输入密码");
     }
 
  
